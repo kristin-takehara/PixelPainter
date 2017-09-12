f48282cb234d7214d1dbb096d119ceed52d265cb
Kristin Takehara <KRISTINTAKEHARA@Kristins-MacBook-Pro.local>
2017-09-11 18:38:02 -1000

    pixel canvas complete

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,1 +1,16 @@
-console.log('Baseem was here');
+//console.log('Baseem was here');
+
+
+//cellDiv to hold all other divs
+var ppCanvas = document.createElement("div");
+ppCanvas.id = "cellDiv";
+pixelPainter.appendChild(ppCanvas);
+
+//forloop to create all pixel boxed as divs
+var cell = Array(2500).fill(1);
+for (var i =0; i < cell.length; i++){
+  cell[i] = document.createElement("div");
+  cell[i].className = "cells";
+  ppCanvas.appendChild(cell[i]);
+}
+
