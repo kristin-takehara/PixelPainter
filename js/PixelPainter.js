986cbbc37f6c1ddc4e875d947fcb300ecd401eff
Kristin Takehara <KRISTINTAKEHARA@Kristins-MacBook-Pro.local>
2017-09-12 12:04:08 -1000

    clear button done

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -2,6 +2,12 @@ var colorArray = ['#9B59B6','#85C1E9','#F1948A','#F1C40F','#76D7C4','#DC7633','#
 var selectedColor;
 var width = 0;
 var height = 0;
+var clearButton = document.createElement("button");
+  clearButton.id = "eraseAll";
+  clearButton.innerHTML = "CLEAR";
+  clearButton.addEventListener("click", clearCanvas);
+  pixelPainter.appendChild(clearButton);
+
 
 //create forloop to generate parent div = row
 
@@ -17,9 +23,10 @@ for(var i = 0; i < width; i++){
     cells.addEventListener("mouseover", changeColor);
     cells.className = "cellsInRow";
     ppCanvas.appendChild(cells);
-  }
-  //this for loop creates the color pallete which is divs in the parent row
+  } 
+  
 }
+//this for loop creates the color pallete which is divs in the parent row
 for (var k = 0; k < colorArray.length; k++){
   var colorCanvas = document.createElement('div');
   colorCanvas.style.backgroundColor = colorArray[k];
@@ -27,13 +34,11 @@ for (var k = 0; k < colorArray.length; k++){
   colorCanvas.className = "paintParentRow";
   pixelPainter.appendChild(colorCanvas);
 }
+
 }
 divCanvas(51,51);
 
 //create new set of parent and child divs for colors:
-
-
-
 function changeColor(){
   event.target.style.backgroundColor = selectedColor;
 }
@@ -41,4 +46,12 @@ function changeColor(){
 
 function selectColor(){
   selectedColor = event.target.style.backgroundColor;
+}
+
+//set cells to background white
+function clearCanvas(){
+  cells = document.getElementsByClassName("cellsInRow");
+  for (var i =0; i < cells.length; i++){
+    cells[i].style.backgroundColor = 'fdfefe';
+  }
 }
